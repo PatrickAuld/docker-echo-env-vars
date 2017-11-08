720c2d5411183c343f2af3ee0648fa0366a6e9a4
Patrick Auld <Patrick.Auld@PlanGrid.com>
2017-11-07 17:11:56 -0800

    Adds whitelist of envs

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': '/'})
 @app.route('/<path:path>')
 def hello_world(path):
+    envs = os.environ
+    if os.getenv("ENV_WHITELIST"):
+        whitelist = os.getenv("ENV_WHITELIST").split(",")
+        envs = {k: v for (k, v) in envs.items() if k in whitelist}
     return render_template('index.html',
-                           envs=os.environ,
+                           envs=envs,
                            path=path)
 
 
